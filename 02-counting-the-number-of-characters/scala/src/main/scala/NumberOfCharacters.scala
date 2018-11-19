3e2ba64bc9ed7c432178c50543409cecbb9aec36
Yitzhak Dashevsky <idashevsky@gmail.com>
2018-11-19 17:00:08 +0200

    ˆ

--- 02-counting-the-number-of-characters/scala/src/main/scala/NumberOfCharacters.scala
+++ 02-counting-the-number-of-characters/scala/src/main/scala/NumberOfCharacters.scala
@@ -1,6 +1,6 @@
 object NumberOfCharacters extends App {
   var input = ""
-  while (input.length() == 0){
+  while (input.isEmpty()){
     input = scala.io.StdIn.readLine("What is the input string? ")
   }
   println(s"$input has ${input.length()} characters.")
