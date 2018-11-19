fd48e3b257b4711c79651c458e47992a265c3a3b
Yitzhak Dashevsky <idashevsky@gmail.com>
2018-11-19 17:50:13 +0200

    number of characters - FP approach

---
+++ 02-counting-the-number-of-characters/scala/src/main/scala/NumOfCharactersFunc.scala
@@ -0,0 +1,13 @@
+object NumOfCharactersFunc extends App{
+  def whileLoop(cond: => Boolean)(block: => Unit): Unit =
+    if (cond) {
+      block
+      whileLoop(cond)(block)
+    }
+
+  var input = ""
+  whileLoop(input.isEmpty()) {
+    input = scala.io.StdIn.readLine("What is the input string, Bro? ")
+  }
+  println(s"$input has ${input.length()} characters.")
+}
--- 02-counting-the-number-of-characters/scala/src/main/scala/NumberOfCharacters.scala
+++ 02-counting-the-number-of-characters/scala/src/main/scala/NumberOfCharacters.scala
@@ -1,7 +1,6 @@
 object NumberOfCharacters extends App {
   var input = ""
-  while (input.isEmpty()){
+  while (input.isEmpty())
     input = scala.io.StdIn.readLine("What is the input string? ")
-  }
   println(s"$input has ${input.length()} characters.")
 }
